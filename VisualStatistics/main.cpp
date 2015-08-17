35bf192032e9e4820d45f06e175a09d1f6871371
Kery Wu <kery.wu@qq.com>
2015-08-17 11:50:03 +0800

    Set style to fusion

--- VisualStatistics/main.cpp
+++ VisualStatistics/main.cpp
@@ -3,12 +3,14 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setStyle(QStyleFactory::create(QStringLiteral("fusion")));
+
     QApplication a(argc, argv);
 
     // Call these functions so that we can use default constructor of
     // QSettings
     QCoreApplication::setOrganizationName("Nokia");
-    QCoreApplication::setApplicationName("VisualIntStat");
+    QCoreApplication::setApplicationName("VisualStatistics");
 
     MainWindow w;
     w.show();
