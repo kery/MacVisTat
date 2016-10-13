80210893d01cf8664f7ed219d9417c0a1f27f02a
Kery <kery.wu1@gmail.com>
2016-10-13 14:32:34 +0800

    Implement user report on server side

--- installer/reposerver/src/reposerver/reposerver.go
+++ installer/reposerver/src/reposerver/reposerver.go
@@ -2,11 +2,18 @@ package main
 
 import (
     "net/http"
+    "log"
 )
+
+func userReportHandler(w http.ResponseWriter, r *http.Request) {
+    hostName := r.PostFormValue("host")
+    log.Printf("%s started the client", hostName)
+}
 
 func main() {
     fs := http.FileServer(http.Dir("."))
     http.Handle("/", fs)
+    http.HandleFunc("/report", userReportHandler)
 
     http.ListenAndServe(":4099", nil)
 }
