946204ba0f9efabdd2962cb1fbb52f00650c9930
Kery <kery.wu@nokia-sbell.com>
2021-02-25 21:11:31 +0800

    Add statistics for static file handler.

--- installer/reposerver/src/reposerver/reposerver.go
+++ installer/reposerver/src/reposerver/reposerver.go
@@ -1,28 +1,61 @@
 package main
 
 import (
-    "net/http"
-    "log"
-    "os"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"text/tabwriter"
 )
 
-func userReportHandler(w http.ResponseWriter, r *http.Request) {
-    hostName := r.PostFormValue("host")
-    productType := r.PostFormValue("pt")
-    version := r.PostFormValue("ver")
+var mutex sync.Mutex
+var fileStat map[string]uint32
 
-    file, err := os.OpenFile("reposerver.log", os.O_WRONLY | os.O_APPEND | os.O_CREATE, 0644)
-    if err == nil {
-        defer file.Close()
-        log.SetOutput(file)
-        log.Printf("%s started the client (%s, %s)", hostName, productType, version)
-    }
+type dirWithStat string
+
+func (d dirWithStat) Open(name string) (http.File, error) {
+	file, err := http.Dir(d).Open(name)
+	if err == nil {
+		mutex.Lock()
+		fileStat[name] += 1
+		mutex.Unlock()
+	}
+	return file, err
+}
+
+func userReportHandler(w http.ResponseWriter, r *http.Request) {
+	hostName := r.PostFormValue("host")
+	productType := r.PostFormValue("pt")
+	version := r.PostFormValue("ver")
+
+	file, err := os.OpenFile("reposerver.log", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	if err == nil {
+		defer file.Close()
+		log.SetOutput(file)
+		log.Printf("%s started the client (%s, %s)", hostName, productType, version)
+	}
+}
+
+func fileStatHandler(w http.ResponseWriter, r *http.Request) {
+	tw := tabwriter.NewWriter(w, 0, 0, 4, ' ', 0)
+
+	mutex.Lock()
+	for name, count := range fileStat {
+		fmt.Fprintf(tw, "%s: \t%d\n", name, count)
+	}
+	mutex.Unlock()
+
+	tw.Flush()
 }
 
 func main() {
-    fs := http.FileServer(http.Dir("."))
-    http.Handle("/", fs)
-    http.HandleFunc("/report", userReportHandler)
+	fileStat = make(map[string]uint32)
 
-    http.ListenAndServe(":4099", nil)
+	fs := http.FileServer(dirWithStat("."))
+	http.Handle("/", fs)
+	http.HandleFunc("/report", userReportHandler)
+	http.HandleFunc("/fstat", fileStatHandler)
+
+	http.ListenAndServe(":4099", nil)
 }
